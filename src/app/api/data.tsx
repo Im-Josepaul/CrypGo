616d97a18192ba1e7ed72e77d74e8bb10723720d
Josepaul Jenson <64831240+Im-Josepaul@users.noreply.github.com>
2025-07-11 19:05:56 +0530

    Update data.tsx
    Added  public path to the icons

--- src/app/api/data.tsx
+++ src/app/api/data.tsx
@@ -11,7 +11,7 @@ export const pricedeta: {
   {
     title: "Bitcoin",
     short: "BTC",
-    icon: "/images/icons/icon-bitcoin.png",
+    icon: "public/images/icons/icon-bitcoin.png",
     background: "bg-warning bg-opacity-20",
     mark: "$94,040.99 (-0.9%)",
     width: 18,
@@ -21,7 +21,7 @@ export const pricedeta: {
   {
     title: "Ethereum",
     short: "ETH",
-    icon: "/images/icons/icon-ethereum.png",
+    icon: "public/images/icons/icon-ethereum.png",
     background: "bg-light_grey",
     mark: "$4,878.26 (-35.9%)",
     width: 18,
@@ -31,7 +31,7 @@ export const pricedeta: {
   {
     title: "Litecoin",
     short: "LTC",
-    icon: "/images/icons/icon-litecoin.png",
+    icon: "public/images/icons/icon-litecoin.png",
     background: "bg-light_grey",
     mark: "$410.26 (-79.1%)",
     width: 18,
@@ -41,7 +41,7 @@ export const pricedeta: {
   {
     title: "Solana",
     short: "SOL",
-    icon: "/images/icons/icon-solana.png",
+    icon: "public/images/icons/icon-solana.png",
     background: "bg-light_grey",
     mark: "$259.96 (-8.2%)",
     width: 24,
@@ -51,7 +51,7 @@ export const pricedeta: {
   {
     title: "Binance",
     short: "BNB",
-    icon: "/images/icons/icon-binance.png",
+    icon: "public/images/icons/icon-binance.png",
     background: "bg-light_grey",
     mark: "$0.7316 (-46.2%)",
     width: 46,
@@ -61,7 +61,7 @@ export const pricedeta: {
   {
     title: "Arbitrum",
     short: "ARB",
-    icon: "/images/icons/icon-arbitrum.png",
+    icon: "public/images/icons/icon-arbitrum.png",
     background: "bg-light_grey",
     mark: "$0.7316 (-46.2%)",
     width: 46,
