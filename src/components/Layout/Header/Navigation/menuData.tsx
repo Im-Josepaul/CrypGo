b9fb04f70e2fba37d870c05003d52acddf927c6e
Josepaul Jenson <64831240+Im-Josepaul@users.noreply.github.com>
2025-07-12 13:23:03 +0530

    Update menuData.tsx
    Added GitHub Link

--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -3,5 +3,5 @@ import { HeaderItem } from "../../../../types/menu";
 export const headerData: HeaderItem[] = [
   { label: "Buy Crypto", href: "/#main-banner" },
   { label: "How it works", href: "/#development" },
-  { label: "GitHub", href: "https://github.com/im-Josepaul/" }
+  { label: "GitHub", href: "https://github.com/Im-Josepaul/CrypGo" }
 ];
